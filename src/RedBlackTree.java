adb5750a7232646d06ea62eb22b51dbe7e28ab0f
diaz1202 <alejodiaz.1202@gmail.com>
2014-10-03 13:14:21 -0600

    Cambio

---
+++ src/RedBlackTree.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alejo
+ */
+import java.util.Iterator;
+public class RedBlackTree implements WordSet {
+
+    @Override
+    public void add(Word wordObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Word get(Word word) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/WordSetFactory.java
@@ -0,0 +1,49 @@
+/*
+UVG
+Algoritmos y Estructuras de Datos - 2011
+Hoja de trabajo 7 
+Autor: Eduardo Castellanos
+
+Descripci�n: WordSetFactory. Clase  utilizada para instanciar los diferentes tipos de sets. 
+*/
+import java.util.HashMap;
+import java.util.TreeMap;
+class WordSetFactory {
+	
+	// Metodo que genera un objeto que implementa WordSet
+	// parametro tipo: 1 = SimpleSet
+	//                         2 = implementado con Red black tree
+	//                         3 = implementado con Splay Tree
+	//                         4 = implementado con Hash Table
+	//                         5 = implementado con TreeMap (de Java Collection Framework)
+	
+	public static WordSet generateSet(int tipo)
+	{
+	    if (tipo == 1)
+		    return new SimpleSet();
+	else
+            {
+                if (tipo == 2 )
+                {
+                    return new SplayTree();
+                }
+                if (tipo == 3)
+                {
+                    return new RedBlackTree(); 
+                }
+                if (tipo == 4)
+                {
+                    return (WordSet) new HashMap();
+                }
+                
+                if (tipo ==5)
+                {
+                    return (WordSet) new TreeMap();
+                }
+                        
+            }		
+            return null;		
+	}
+}
+	
+	
