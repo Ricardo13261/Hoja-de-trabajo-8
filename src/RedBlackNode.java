3cc2bc50f7ddc9b4b29f8a7d60c1229133fae6aa
Ricardo13261 <Ricardo13261@users.noreply.github.com>
2014-10-06 18:13:25 -0600

    Create RedBlackNode.java

---
+++ src/RedBlackNode.java
@@ -0,0 +1,32 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Profesor: Douglas Barrientos
+ *Autores:
+ *    Ricardo Franco, 13261
+ *    Aejandro Díaz, 13082
+ *    Jenry
+ * Esta clase crea un nodo para la implementación redBlackTree
+ */ 
+public class RedBlackNode
+    {
+            // Constructors
+        RedBlackNode( Comparable theElement )
+        {
+            this( theElement, null, null );
+        }
+
+        RedBlackNode( Comparable theElement, RedBlackNode lt, RedBlackNode rt )
+        {
+            element  = theElement;
+            left     = lt;
+            right    = rt;
+            color    = RedBlackTree.BLACK;
+        }
+
+            // Friendly data; accessible by other package routines
+        Comparable   element;    // The data in the node
+        RedBlackNode left;       // Left child
+        RedBlackNode right;      // Right child
+        int          color;      // Color
+    }
